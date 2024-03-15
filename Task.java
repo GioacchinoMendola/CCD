45142df4d4d015644f67cc5cd485f6621a924722
GioacchinoMendola <gioacchino.mendola@gmail.com>
2024-03-15 14:20:59 +0100

    Create Task.java

---
+++ Task.java
@@ -0,0 +1,7 @@
+public interface Task {
+    String getTitle();
+    void setTitle(String title);
+    String getDescription();
+    void setDescription(String description);
+    // Otros métodos de la interfaz Task
+}
